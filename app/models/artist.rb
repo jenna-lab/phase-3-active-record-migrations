5ad417b15fc8e17e8d0bd4f97af791bc6dfde73c
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-02-23 11:47:30 +0300

    complete

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,10 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+
+    def initialize(id,name,genre,age,hometown)
+        @id = id
+        @name = name
+        @genre = genre
+        @hometown = hometown
+    end
+  end
